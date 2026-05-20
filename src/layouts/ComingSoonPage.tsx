a41677682c10fa7ba5c1f91ed347db1def13cf2b
Taha Rashid <taharashid710192@gmail.com>
2026-05-19 21:05:46 -0400

    Update ComingSoonPage.tsx

--- src/layouts/ComingSoonPage.tsx
+++ src/layouts/ComingSoonPage.tsx
@@ -1,8 +1,11 @@
 export default function ComingSoonPage() {
 
     return (
-        <div className="h-full">
-            Case study coming soon!
+        <div className="flex items-center justify-center h-screen">
+            <span className="bowlby-one-regular text-7xl">
+                Case study 
+                    <span className="underline decoration-[#FB8500]">coming soon!</span>
+                </span>
         </div>
     );
 }
